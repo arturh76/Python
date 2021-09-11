b243893251cfb18f749de81fccd601a1dbf4f31a
arturh76 <arturh76@gmail.com>
2021-09-11 14:17:06 +0300

    Task_3

---
+++ Khizriev_Artur_dz_3.py
@@ -0,0 +1,7 @@
+percent = int(input('Введите число от 1 до 100; '))
+if percent == 1:
+    print(percent, 'процент')
+elif percent < 5:
+    print(percent, "процента")
+elif percent < 100:
+    print(percent, "процентов")
