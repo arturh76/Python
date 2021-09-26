386be1b1f53f2f6c4c0fc3a1b93b9c40af93f660
arturh76 <arturh76@gmail.com>
2021-09-27 01:13:04 +0300

    Task_2

--- Khizriev_Artur_dz_2.py
+++ Khizriev_Artur_dz_2.py
@@ -1,11 +1,11 @@
 # инициализация переменных
-my_temp = ''
 my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
 for i in my_list:
+    i = i.replace("+", "").replace("-", '')
     if i.isdigit() == True and int(i) < 10:
         print('"0{i}"'.format(i=i), end=" ")
         if i.isdigit() == True and int(i) > 10:
             print('{i}'.format(i=i), end=" ")
     else:
-        print('"{i}"'.format(i=i), end=" ")
+            print('"{i}"'.format(i=i), end=" ")
